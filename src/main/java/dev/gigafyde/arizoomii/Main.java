57ef963232bc5765059f3ebf0b0b62f4767883a4
Laurant <laurant@gigafyde.dev>
2020-11-15 00:19:16 +0100

    Working bot :)

---
+++ src/main/java/dev/gigafyde/arizoomii/Main.java
@@ -0,0 +1,22 @@
+package dev.gigafyde.arizoomii;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static JDA jda;
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws Exception {
+        jda = JDABuilder.createDefault(System.getenv("TOKEN"))
+                .addEventListeners(new ReactionListener())
+                .build();
+        jda.awaitReady();
+        ReactionListener.youtubePing = jda.getRoleById("766970231498866699");
+        ReactionListener.twitchPing = jda.getRoleById("766970181447581696");
+        ReactionListener.announcementPing = jda.getRoleById("766970037352398861");
+        log.info("Starting arizoomii");
+    }
+}
---
+++ src/main/java/dev/gigafyde/arizoomii/ReactionListener.java
@@ -0,0 +1,37 @@
+package dev.gigafyde.arizoomii;
+
+import net.dv8tion.jda.api.entities.Emote;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class ReactionListener extends ListenerAdapter {
+    public static Role youtubePing;
+    public static Role twitchPing;
+    public static Role announcementPing;
+
+    @Override
+    public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
+        if (!event.getChannel().getId().equals("752523144853585980")) return;
+        if (event.getReactionEmote().getName().equals("youtube"))
+            event.getGuild().addRoleToMember(event.getMember(), youtubePing).reason("YT Reaction Role").queue();
+        if (event.getReactionEmote().getName().equals("twitch"))
+            event.getGuild().addRoleToMember(event.getMember(), twitchPing).reason("Twitch Reaction Role").queue();
+        if (event.getReactionEmote().getName().equals("neongoogletada"))
+            event.getGuild().addRoleToMember(event.getMember(), announcementPing).reason("Announcement Reaction Role").queue();
+    }
+
+    @Override
+    public void onGuildMessageReactionRemove(GuildMessageReactionRemoveEvent event) {
+        Member member = event.getGuild().retrieveMemberById(event.getUserId()).complete();
+        if (!event.getChannel().getId().equals("752523144853585980")) return;
+        if (event.getReactionEmote().getName().equals("youtube"))
+            event.getGuild().removeRoleFromMember(member, youtubePing).reason("YT Reaction Role").queue();
+        if (event.getReactionEmote().getName().equals("twitch"))
+            event.getGuild().removeRoleFromMember(member, twitchPing).reason("Twitch Reaction Role").queue();
+        if (event.getReactionEmote().getName().equals("neongoogletada"))
+            event.getGuild().removeRoleFromMember(member, announcementPing).reason("Announcement Reaction Role").queue();
+    }
+}
